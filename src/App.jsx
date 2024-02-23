b93bca98509d596cabe1803a8429866e92ae1f14
stephenbliz <uzomaebuka11@gmail.com>
2024-02-23 20:46:57 +0100

    added the project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import About from "./components/about/About"
 import Header from "./components/header/header"
 import Hero from "./components/hero/Hero"
+import Project from "./components/projects/Project"
 import Skills from "./components/skills/Skills"
 
 
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <Project />
     </>
   )
 }
