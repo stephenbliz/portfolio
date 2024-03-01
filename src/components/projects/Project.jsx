64b16b9eb94fe646bb6e4602a8cdb9d2d54a4965
stephenbliz <uzomaebuka11@gmail.com>
2024-03-01 20:44:46 +0100

    Added media query to the project section and made some modifications in the about and header media query

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -10,12 +10,12 @@ const Project = () => {
     
     const projects = [
         {name: 'expense tracker', desc: 'This web application calculates all your expenses and incomes and give the balance by substracting the expenses from the income', 
-            image: expense, link: 'https://expense-tracker-theta-henna.vercel.app', color: '', id: 1},
-        {name: 'text editor', desc: 'A web application that allow users to create, edit, and manipulate plain text files', image: editor, link: 'https://text-editor-app-stephenbliz.vercel.app', color: '', id: 2},
-        {name: 'portnew project', desc: 'A random web design suggestion because of the interesting challenges in it', image: portnew, link: 'https://portnew-project.vercel.app', color: '', id: 3},
-        {name: 'BMI calculator', desc: 'This application allow users to calculate their Body Mass Index by inputing their height and weight', image: bmi, link: 'https://bmi-calculator.khaki.iota.vercel.app', color: '', id: 4},
-        {name: 'age calculator', desc: 'Users can calculate their age with the help of this web application by inputing the date, month and year of birth', image: ageCalculator, link: 'https://age-calculator-nine-eta.vercel.app', color: '', id: 5},
-        {name: 'live language translator', desc: 'This web application allow users to translate foreign languages to native language', image: translator, link: 'https://live-language-translator.vercel.app', color: '', id: 6},
+            image: expense, link: 'https://expense-tracker-theta-henna.vercel.app', color: '#ab6f60', id: 1},
+        {name: 'text editor', desc: 'A web application that allow users to create, edit, and manipulate plain text files', image: editor, link: 'https://text-editor-app-stephenbliz.vercel.app', color: '#b8b4b4', id: 2},
+        {name: 'portnew project', desc: 'A random web design suggestion because of the interesting challenges in it', image: portnew, link: 'https://portnew-project.vercel.app', color: '#2e7941', id: 3},
+        {name: 'BMI calculator', desc: 'This application allow users to calculate their Body Mass Index by inputing their height and weight', image: bmi, link: 'https://bmi-calculator.khaki.iota.vercel.app', color: '#afccda', id: 4},
+        {name: 'age calculator', desc: 'Users can calculate their age with the help of this web application by inputing the date, month and year of birth', image: ageCalculator, link: 'https://age-calculator-nine-eta.vercel.app', color: '#8951b7', id: 5},
+        {name: 'live language translator', desc: 'This web application allow users to translate foreign languages to native language', image: translator, link: 'https://live-language-translator.vercel.app', color: '#6b6b8a', id: 6},
     ];
 
     return (
@@ -27,7 +27,7 @@ const Project = () => {
                         <div className={style.project__front}>
                             <img src={project.image} alt="project image" className={style.project__image} />
                         </div>
-                        <div className={style.project__back}>
+                        <div className={style.project__back} style={{backgroundColor: project.color}}>
                             <h3 className={style.project__name}>{project.name}</h3>
                             <p className={style.project_desc}>
                                 {project.desc}
