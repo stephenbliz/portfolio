a0e2fa300087539a23301c2ecfad73386d1dc840
stephenbliz <uzomaebuka11@gmail.com>
2024-03-11 11:50:48 +0100

    modified the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-
-
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { resolve } from 'path';
-import dotenv from 'dotenv';
 
-// Load environment variables from .env
-dotenv.config();
-
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
 export default defineConfig({
   plugins: [react()],
-  
-});
+})
+
