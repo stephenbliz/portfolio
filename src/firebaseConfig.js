f6f78ea87484e7faaf53c0676b53102f9aabfc2c
stephenbliz <uzomaebuka11@gmail.com>
2024-03-11 13:30:37 +0100

    modified firebaseConfig file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,14 +3,25 @@ import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage';
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID,
+//   measurementId: import.meta.env.VITE_MEASUREMENT_ID
+// };
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+  apiKey: "AIzaSyBwfMS2BstsfHvAVae8dg2cmfQaxzsz41U",
+  authDomain: "portfolio-3027d.firebaseapp.com",
+  projectId: "portfolio-3027d",
+  storageBucket: "portfolio-3027d.appspot.com",
+  messagingSenderId: "799847408800",
+  appId: "1:799847408800:web:997f752cd8cf7bdb89d5c5",
+  measurementId: "G-Q63WNPDV04"
 };
 
 const app = initializeApp(firebaseConfig);
