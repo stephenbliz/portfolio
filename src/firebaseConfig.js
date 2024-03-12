e4ebd0ee297ef073a883f1a7a1fc935aa4181b75
stephenbliz <uzomaebuka11@gmail.com>
2024-03-12 12:05:29 +0100

    comment deleted

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -13,17 +13,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBwfMS2BstsfHvAVae8dg2cmfQaxzsz41U",
-//   authDomain: "portfolio-3027d.firebaseapp.com",
-//   projectId: "portfolio-3027d",
-//   storageBucket: "portfolio-3027d.appspot.com",
-//   messagingSenderId: "799847408800",
-//   appId: "1:799847408800:web:997f752cd8cf7bdb89d5c5",
-//   measurementId: "G-Q63WNPDV04"
-// };
-
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
