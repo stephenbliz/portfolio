e41449a5541b787ad79cfa4ded2c37733dca4f2a
stephenbliz <uzomaebuka11@gmail.com>
2024-03-12 19:11:38 +0100

    updated the bmi project link

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -13,7 +13,7 @@ const Project = () => {
             image: expense, link: 'https://expense-tracker-theta-henna.vercel.app', color: '#ab6f60', id: 1},
         {name: 'text editor', desc: 'A web application that allow users to create, edit, and manipulate plain text files', image: editor, link: 'https://text-editor-app-stephenbliz.vercel.app', color: '#b8b4b4', id: 2},
         {name: 'portnew project', desc: 'A random web design suggestion because of the interesting challenges in it', image: portnew, link: 'https://portnew-project.vercel.app', color: '#2e7941', id: 3},
-        {name: 'BMI calculator', desc: 'This application allow users to calculate their Body Mass Index by inputing their height and weight', image: bmi, link: 'https://bmi-calculator.khaki.iota.vercel.app', color: '#afccda', id: 4},
+        {name: 'BMI calculator', desc: 'This application allow users to calculate their Body Mass Index by inputing their height and weight', image: bmi, link: 'https://bmi-calculator-khaki-iota.vercel.app', color: '#afccda', id: 4},
         {name: 'age calculator', desc: 'Users can calculate their age with the help of this web application by inputing the date, month and year of birth', image: ageCalculator, link: 'https://age-calculator-nine-eta.vercel.app', color: '#8951b7', id: 5},
         {name: 'live language translator', desc: 'This web application allow users to translate foreign languages to native language', image: translator, link: 'https://live-language-translator.vercel.app', color: '#6b6b8a', id: 6},
     ];
