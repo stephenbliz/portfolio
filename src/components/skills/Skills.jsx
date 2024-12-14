5b53acbeb6c758958ecd490bfd23ecfaa68fe4a5
bliz <uzomaebuka11@gmail.com>
2024-12-14 12:06:18 +0100

    Updated my skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -4,7 +4,10 @@ import { FaHtml5 } from "react-icons/fa";
 import { IoLogoCss3 } from "react-icons/io5";
 import { DiJavascript } from "react-icons/di";
 import { FaReact } from "react-icons/fa";
-import { IoLogoFirebase } from "react-icons/io5";
+import { RiNextjsLine } from "react-icons/ri";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { TbSquareLetterG } from "react-icons/tb";
+import { SiChakraui } from "react-icons/si";
 
 const skillsVariants = {
     hidden: {
@@ -31,7 +34,10 @@ const Skills = () => {
         {name: 'CSS', icon: <IoLogoCss3 className={style.skill__icon} style={{color: '#264de4'}}/>, id: 2},
         {name: 'Javascript', icon: <DiJavascript className={style.skill__icon} style={{color: '#f0db4f'}}/>, id: 3},
         {name: 'React', icon: <FaReact className={style.skill__icon} style={{color: '#61dbfb'}}/>, id: 4},
-        {name: 'Firebase', icon: <IoLogoFirebase className={style.skill__icon} style={{color: '#FFA611'}}/>, id: 5}
+        {name: 'Nextjs', icon: <RiNextjsLine className={style.skill__icon} style={{color: '#ffffff'}}/>, id: 5},
+        {name: 'TailwindCss', icon: <RiTailwindCssFill className={style.skill__icon} style={{color: '#36B6F2'}}/>, id: 6},
+        {name: 'ChakraUI', icon: <SiChakraui className={style.skill__icon} style={{color: '#28B9AD'}}/>, id: 7},
+        {name: 'Hygraph', icon: <TbSquareLetterG className={style.skill__icon} style={{color: '#ffffff'}}/>, id: 8},
     ]
 
     return (
